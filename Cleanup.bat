95e7d08398b12068bf6ae3a7ffa203ece080d196
bonina <bonina@users.noreply.github.com>
2023-05-12 12:27:07 -0400

    Update Cleanup.bat

--- Cleanup.bat
+++ Cleanup.bat
@@ -6,44 +6,55 @@ echo  for Windows(R) 7/8/10/11
 echo ==========================
 echo.
 
+:: Check admin privileges
 openfiles.exe >nul 2>&1
 if %errorlevel% neq 0 (
 echo ERROR please execute with admin privileges
 goto :error
 )
 
+:: Detect safe mode
 color 0A
 if not "%safeboot_option%"=="" (
 echo Safe mode detected & echo.
 )
 
-echo (new ActiveXObject("WScript.Shell")).AppActivate("Disk Cleanup"); > %temp%\focus.js
+:: Create temporary appactivate script file to bring cleanmgr windows to foreground 
+echo (new ActiveXObject("WScript.Shell")).AppActivate("Disk Cleanup"); > %temp%\focus.js || (echo Cleanup ERROR - create temp ERROR & goto :error)
 
-echo Cleanup setup - please select cleaning categories & cleanmgr.exe /sageset:1 && (echo Cleanup setup OK) || (echo Cleanup setup ERROR & goto :error)
+:: Cleanmgr categories selection and save
+echo Cleanup setup - please select cleaning categories & cleanmgr.exe /sageset:1 && echo Cleanup setup OK || (echo Cleanup setup ERROR & goto :error)
+
+:: Cleanmgr cleanup task
 echo. & echo Cleanup in progress...
-
 start /b cmd /c "cleanmgr.exe /sagerun:1"
 
+:: Error in case previous task is unsuccessful
 if %errorlevel% neq 0 (
 echo Cleanup ERROR
 goto :error
 )
 
+:: Focus loop to bring cleanmgr windows to foreground 
 :repeat
 timeout /t 1 >nul 2>&1
 cscript //nologo %temp%\focus.js >nul 2>&1
 tasklist | findstr "cleanmgr.exe" >nul 2>&1 && goto :repeat
 
 echo Cleanup OK
-del %temp%\focus.js >nul 2>&1 || (echo Cleanup ERROR - del temp ERROR & goto :error)
+:: Delete previously created temporary appactivate script file
+if exist %temp%\focus.js del %temp%\focus.js >nul 2>&1 || (echo Cleanup ERROR - clear temp ERROR & goto :error)
 
+:: Skip older versions of Windows without dism tool
 ver | find " 6." >nul 2>&1
-if %errorlevel% equ 0 goto old
+if %errorlevel% equ 0 goto skip
 
+:: Dism cleanup task
 echo. & echo WinSxS folder cleanup in progress...
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase >nul 2>&1 && (echo WinSxS folder cleanup OK) || (echo WinSxS folder cleanup ERROR & goto :error)
 
-:old
+:: Stop Windows Update process before deleting the update cache
+:skip
 echo. & echo Windows update cache cleanup in progress...
 sc query wuauserv | find "STATE" | find "RUNNING" >nul 2>&1
 if %errorlevel% equ 0 (
